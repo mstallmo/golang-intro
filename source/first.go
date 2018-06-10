0996d61ad5d8b315d6a89d781f40c4f28eee1ed0
Mason Stallmo <mstallmo@stats.com>
2018-06-10 12:00:26 -0500

    concise variable declarations

--- source/first.go
+++ source/first.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	var message string = "Hello world.go variable"
-	var a, b, c int = 1, 2, 3
+	message := "Hello world.go variable"
+	a, b, c := 1, 2, 3
 
 	fmt.Println(message, a, b, c)
 }
