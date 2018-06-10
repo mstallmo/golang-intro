b103409e0642a71bf767cdf89f865864252ad3f3
Mason Stallmo <mstallmo@stats.com>
2018-06-10 11:59:20 -0500

    variable declarations

--- source/first.go
+++ source/first.go
@@ -1,7 +1,12 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World.go!")
+	var message string = "Hello world.go variable"
+	var a, b, c int = 1, 2, 3
+
+	fmt.Println(message, a, b, c)
 }
